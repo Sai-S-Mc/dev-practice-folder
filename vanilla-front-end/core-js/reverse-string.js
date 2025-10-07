78f2d3547c1d86d25fb5d0f971c70c196bcad4e0
Sai-S-Mc <sai.satti97@gmail.com>
2025-10-07 15:18:39 -0400

    feat: Reverse a string using a backward loop
    - Loop through the input string from end to start using a classic for loop
    - Append each character to a new output string
    - Return the reversed string
    - Practiced string traversal and character accumulation

---
+++ vanilla-front-end/core-js/reverse-string.js
@@ -0,0 +1,21 @@
+/* Problem  : Write a function that takes a string and returns the reversed version of that string.
+
+Steps:
+1. Loop through the string
+2. Push characters into a new variable in the reverse order of the input
+3. Return the output as a string */
+
+function reverseString(str) {
+  let outputString = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    outputString = outputString + str[i];
+  }
+
+  return outputString;
+}
+
+// Test case
+// Input : string
+console.log(reverseString("hello"));
+// Expected output : olleh
