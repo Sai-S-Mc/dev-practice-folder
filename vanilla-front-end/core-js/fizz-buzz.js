826f54ccf7f96dd0be9cf85fe2133aeff1f6fbcc
Sai-S-Mc <sai.satti97@gmail.com>
2025-10-08 19:40:17 -0400

    refactor: Improve comments and clarity in fizzBuzz solution
    - Reorganized and updated comments for readability

--- vanilla-front-end/core-js/fizz-buzz.js
+++ vanilla-front-end/core-js/fizz-buzz.js
@@ -1,4 +1,4 @@
-/* Problem : Write a function that loops from 1 to 20.
+/* Problem : Write a function that loops from 1 to 20
 For each number:
 - If divisible by 3, print "Fizz"
 - If divisible by 5, print "Buzz"
@@ -10,11 +10,14 @@ Steps:
 2. Generate an output of Fizz/Buzz/FizzBuzz/number based on divisibility
 3. Return the output as an array of numbers and/or strings */
 
+
+// Manually loop through the array without using built-in methods
 function fizzBuzz() {
+  // Returns an array of strings and numbers based on divisibility rules from 1 to 20
+
   let outputArray = [];
 
   for (let i = 1; i <= 20; i++) {
-
     if (i % 3 === 0 && i % 5 === 0) {
       outputArray.push("FizzBuzz");
     } else if (i % 3 === 0) {
@@ -24,12 +27,12 @@ function fizzBuzz() {
     } else {
       outputArray.push(i);
     }
-
   }
   return outputArray;
 }
 
-// Test case
-// Input : none (1-20 range is fixed)
+/* Test case
+Input : none (1-20 range is fixed)
+Expected output : [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz'] */
+
 console.log(fizzBuzz());
-// Expected output : [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz']
