8ef7277c212c45f436ef716140b42909fbac30d0
Sai-S-Mc <sai.satti97@gmail.com>
2025-10-08 20:04:26 -0400

    refactor: Improve comments and output format for reverseString
    Reorganize and update comment structure for readability

--- vanilla-front-end/core-js/reverse-string.js
+++ vanilla-front-end/core-js/reverse-string.js
@@ -1,11 +1,14 @@
-/* Problem  : Write a function that takes a string and returns the reversed version of that string.
+/* Problem  : Write a function that takes a string and returns the reversed version of that string
 
 Steps:
 1. Loop through the string
 2. Push characters into a new variable in the reverse order of the input
 3. Return the output as a string */
 
+// Manually reverse the string without built-in-methods
 function reverseString(str) {
+  // Returns a string that is the input string reversed
+
   let outputString = "";
 
   for (let i = str.length - 1; i >= 0; i--) {
@@ -15,7 +18,8 @@ function reverseString(str) {
   return outputString;
 }
 
-// Test case
-// Input : string
+/* Test case
+Input : string - "hello"
+Expected output : olleh */ 
+
 console.log(reverseString("hello"));
-// Expected output : olleh
