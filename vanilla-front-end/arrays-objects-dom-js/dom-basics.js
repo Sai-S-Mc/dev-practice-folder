ecbd4d054141d630ec30b06ed48fd80161cb9caa
Sai-S-Mc <sai.satti97@gmail.com>
2025-10-22 18:48:26 -0400

    refactor: Add comments to dom-basics.js and dom-input.js
    - Ensure better clarity

--- vanilla-front-end/arrays-objects-dom-js/dom-basics.js
+++ vanilla-front-end/arrays-objects-dom-js/dom-basics.js
@@ -1,6 +1,11 @@
-let headingElement = document.getElementById("heading")
-setTimeout(()=>{
-    headingElement.textContent = "Hi! This is the new heading."
-    headingElement.style.color = "seagreen"
-    headingElement.style.fontSize = "80px"
-}, 3000)
+/* Basic DOM manipulation Challenge
+  Prompt: Select an existing h1 element and update its content and style using JavaScript. */
+
+let headingElement = document.getElementById("heading");
+
+// Dynamically update the heading's content and styling after a delay of 3000ms
+setTimeout(() => {
+  headingElement.textContent = "Hi! This is the new heading.";
+  headingElement.style.color = "seagreen";
+  headingElement.style.fontSize = "80px";
+}, 3000);
--- vanilla-front-end/arrays-objects-dom-js/dom-input.js
+++ vanilla-front-end/arrays-objects-dom-js/dom-input.js
@@ -1,9 +1,13 @@
+/* Real-time DOM updates
+  Dynamically update the page content as the user types */
+
 function handleInput(event) {
   // Updates the content of the 'result' paragraph according to the user input
 
   const resultElement = document.getElementById("result");
-  let input = event.target.value.trim();
+  let input = event.target.value.trim(); // Remove whitespaces
 
+  // Conditionally render result
   if (input) {
     resultElement.textContent = `You typed: ${input}`;
   } else {
