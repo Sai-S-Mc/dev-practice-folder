326b9f4ba89a0c5561aabdc8516cacc29cdf2e0a
Sai-S-Mc <sai.satti97@gmail.com>
2025-10-21 22:00:19 -0400

    feat: extract requirements into an array and render as <li> elements
    - Assign an array of strings to each object’s requirements key
    - Map over the array to wrap each requirement in an <li> element for display

--- vanilla-front-end/script.js
+++ vanilla-front-end/script.js
@@ -1,10 +1,20 @@
+function getRequirements(requirementArray) {
+  return requirementArray
+    .map((eachRequirement) => {
+      return `<li>${eachRequirement}</li>`;
+    })
+    .join("");
+}
+
 function getHtmlCssData(data) {
   return data
     .map((obj) => {
       return `<li>
         <strong>Prompt</strong> : ${obj.prompt} <br />
         <strong>Goal</strong>: ${obj.goal} <br />
-        <strong>Requirements</strong>: ${obj.requirements}<br />
+        <strong>Requirements</strong>: <ul>${getRequirements(
+          obj.requirements
+        )}</ul>
         <strong>Answer</strong>:
         <a href = "${obj.url}" class="functional-link">
           ${obj.urlName}
@@ -16,13 +26,15 @@ function getHtmlCssData(data) {
 
 let htmlCssData = [
   {
-    prompt: "Now for your semantic HTML + box model layou challenge:",
+    prompt: "Now for the semantic HTML + box model layout challenge:",
     goal: "Recreate a simple blog-style layout using only semantic HTML + box model (no Flexbox/Grid yet).",
-    requirements:
-      " Use only semantic HTML tags Style layout using margins, padding, borders, and box-sizing Keep everything in one file (semantic-layout.html)",
+    requirements: [
+      "Use only semantic HTML tags Style layout using margins, padding, borders, and box-sizing",
+      "Keep everything in one file (semantic-layout.html)",
+    ],
     url: "core-html-css/semantic-layout.html",
     urlName: "semantic-layout.html",
-  }
+  },
 ];
 
 let htmlCssElement = document.querySelector("#html-css-section");
